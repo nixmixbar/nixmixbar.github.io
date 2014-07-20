e8f41ff02b484a5c3bc6b78e40187d4581cf8e3b
Konstantin Tarkus <hello@tarkus.me>
2014-07-20 20:09:46 +0400

    Change navbar bottom border color on scroll

--- scripts/main.js
+++ scripts/main.js
@@ -8,13 +8,15 @@
 
     var navbar = $('.navbar').hide().removeClass('hide');
     var menu = $('.nav', navbar);
+    var scrollingSpeed = 700;
 
     // https://github.com/alvarotrigo/fullPage.js/
     $('#fullpage').fullpage({
         anchors: ['home', 'about', 'menu', 'photos', 'contact', 'store'],
         css3: true,
+        scrollingSpeed: scrollingSpeed,
         afterLoad: function (anchorLink, index) {
-            console.log('afterLoad(' + anchorLink + ', ' + index + ')');
+            // console.log('afterLoad(' + anchorLink + ', ' + index + ')');
 
             // Highlight an active menu item
             $('li', menu).map(function (i) {
@@ -26,7 +28,7 @@
             });
         },
         onLeave: function (index, nextIndex, direction) {
-            console.log('onLeave(' + index + ', ' + nextIndex + ', ' + direction + ')');
+            // console.log('onLeave(' + index + ', ' + nextIndex + ', ' + direction + ')');
 
             // Hide menu on the home screen
             if (index = 1 && nextIndex == 2) {
@@ -35,6 +37,11 @@
                 navbar.fadeOut('slow');
             } else {
                 navbar.show();
+                if (nextIndex == 5) {
+                    navbar.animate({borderBottomColor: '#fff4e8'}, scrollingSpeed);
+                } else {
+                    navbar.animate({borderBottomColor: '#6d0000'}, scrollingSpeed);
+                }
             }
         }
     });
